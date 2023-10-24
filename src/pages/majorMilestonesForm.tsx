fedbc026a70a4c7a5d94bfebfd2723fd573d6727
Tessa-P <tessapugh10@gmail.com>
2023-10-24 11:21:57 -0400

    baselined milestones form

--- src/pages/majorMilestonesForm.tsx
+++ src/pages/majorMilestonesForm.tsx
@@ -6,12 +6,20 @@ export const MilestonesForm = () => {
     const [dateOfBirth, setDateOfBirth] = useState("")
     const [country, setCountry] = useState("")
     const [transStart, setTransStart] = useState("")
+    const [transEnd, setTransEnd] = useState("")
+    const [comingOutStart, setComingOutStart] = useState("")
+    const [comingOutEnd, setComingOutEnd] = useState("")
+    const [hrtStart, setHrtStart] = useState("")
 
     const { mutate } = api.form.create.useMutation({
         onSuccess: () => {
             setDateOfBirth("")
             setCountry("")
             setTransStart("")
+            setTransEnd("")
+            setComingOutStart("")
+            setComingOutEnd("")
+            setHrtStart("")
         }
     })
 
@@ -25,16 +33,16 @@ export const MilestonesForm = () => {
                     defaultValue={dateOfBirth} 
                     onChange={(e) => setDateOfBirth(e.target.value)}
                     className = ""
-                /><br />
+                /><br /><br />
 
                 <label htmlFor="country">In which country do you currently reside?</label><br />
                 <input 
                     type="text" 
                     id="country" 
                     defaultValue={country} 
-                    onChange={(e) => setDateOfBirth(e.target.value)}
+                    onChange={(e) => setCountry(e.target.value)}
                     className = "border border-black"
-                /><br />
+                /><br /><br />
 
                 <label htmlFor="transStart">What is the earliest date you realized you were trans?</label><br />
                 <input 
@@ -43,7 +51,44 @@ export const MilestonesForm = () => {
                     defaultValue={transStart} 
                     onChange={(e) => setTransStart(e.target.value)}
                     className = ""
-                /><br />
+                /><br /><br />
+
+                <label htmlFor="transEnd">What is the latest date you realized you were trans?</label><br />
+                <input 
+                    type="date" 
+                    id="transEnd" 
+                    defaultValue={transEnd} 
+                    onChange={(e) => setTransEnd(e.target.value)}
+                    className = ""
+                /><br /><br />
+
+                <label htmlFor="comingOutStart">When is the earliest date you came out?</label><br />
+                <input 
+                    type="date" 
+                    id="comingOutStart" 
+                    defaultValue={comingOutStart} 
+                    onChange={(e) => setComingOutStart(e.target.value)}
+                    className = ""
+                /><br /><br />
+
+                <label htmlFor="comingOutEnd">When is the latest date you came out?</label><br />
+                <input 
+                    type="date" 
+                    id="comingOutEnd" 
+                    defaultValue={comingOutEnd} 
+                    onChange={(e) => setComingOutEnd(e.target.value)}
+                    className = ""
+                /><br /><br />
+
+                <label htmlFor="hrtStart">On which day did you start HRT?</label><br />
+                <input 
+                    type="date" 
+                    id="hrtStart" 
+                    defaultValue={hrtStart} 
+                    onChange={(e) => setHrtStart(e.target.value)}
+                    className = ""
+                /><br /><br />
+
 
                 <button 
                     type="button" 
@@ -51,7 +96,11 @@ export const MilestonesForm = () => {
                     onClick={() => mutate({ 
                         dateOfBirth, 
                         country,
-                        transStart })}>
+                        transStart,
+                        transEnd,
+                        comingOutStart,
+                        comingOutEnd,
+                        hrtStart })}>
                         Submit
                 </button>
             </div>
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -29,18 +29,30 @@ export const formRouter = createTRPCRouter({
     .input(z.object({
       dateOfBirth: z.string(),
       country: z.string(),
-      transStart: z.string()
+      transStart: z.string(),
+      transEnd: z.string(),
+      comingOutStart: z.string(),
+      comingOutEnd: z.string(),
+      hrtStart: z.string()
     }))
     .mutation(({ input }) => {
       const dateOfBirth = input.dateOfBirth
       const country = input.country
       const transStart = input.transStart
+      const transEnd = input.transEnd
+      const comingOutStart = input.comingOutStart
+      const comingOutEnd = input.comingOutEnd
+      const hrtStart = input.hrtStart
 
       return db.formData.create({
         data: {
-          dateOfBirth: dateOfBirth,
-          country: country,
-          transStart: transStart
+          dateOfBirth,
+          country,
+          transStart,
+          transEnd,
+          comingOutStart,
+          comingOutEnd,
+          hrtStart,
         }
       })
     })
