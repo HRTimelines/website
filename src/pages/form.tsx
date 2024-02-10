7d8a18ebb246a05d424dcf6c5e3ac9cf4a28a237
Tessa-P <tessapugh10@gmail.com>
2024-02-09 20:45:43 -0500

    form open

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -9,10 +9,6 @@ import MedicationTable from "./components/medications";
 import { NextSectionButton } from "./components/nextSectionButton";
 
 export const Form = () => {
-  const options = ["pills", "patches", "injections", "4th thing"];
-  const [method, setMethod] = useState("");
-
-  const [mascMedicationData, setMascMedicationData] = useState(medicationData);
   return (
     <>
       <div>
